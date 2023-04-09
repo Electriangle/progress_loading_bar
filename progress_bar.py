cda2fd9e7c981d3f5547f1460df9ae27bea7f7ad
Electriangle <90885400+Electriangle@users.noreply.github.com>
2023-04-09 16:15:26 -0400

    Add files via upload

---
+++ progress_bar.py
@@ -0,0 +1,10 @@
+import time
+
+def print_progress_bar(progress, end):
+    print("[{0}{1}] {2}%".format("█" * progress, "-" * (end - progress), progress), end="\r")
+    if progress == end:
+        print()
+
+for i in range(0, 101):
+    print_progress_bar(i, 100)
+    time.sleep(0.05)
